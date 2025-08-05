74eb4549acae198916f06ffac6a1738406941969
Joaquin Stur <joaquinstur@gmail.com>
2025-08-05 20:49:36 -0300

    Crea Hero
    con una fotito y todo che

---
+++ project/src/components/Hero.jsx
@@ -0,0 +1,34 @@
+export function Hero (){
+  return(
+    <section  className="
+    bg-[url('/images/Hero2.png')]
+    bg-cover
+    bg-center
+    bg-no-repeat
+    min-h-screen
+    flex
+    items-center
+    justify-center">
+      <div className="flex flex-col md:flex-row w-full max-w-2xl mx-auto">
+        <div className="border border-white/15 ring-1 ring-white/10 bg-white/10 backdrop-blur-md p-6 rounded-lg w-full md:w-[400px] h-[400px] flex flex-col justify-center items-center text-center">
+          <h2 className="text-4xl md:text-5xl font-bold text-white mb-4">Pesca como un campeón!</h2>
+          <p className="text-lg md:text-xl font-bold text-white mb-8">Guiado por profesionales</p>
+          <a href="" className="
+            inline-block
+            bg-yellow-600
+            text-white 
+            px-6 
+            py-3 
+            rounded-lg 
+            text-lg 
+            font-medium 
+            hover:bg-yellow-700 
+            transition
+          "
+          >Ver excursiones</a>
+        </div>
+      </div>
+      <div className="w-1/2 hidden md:block"></div>
+    </section>
+  )
+}
