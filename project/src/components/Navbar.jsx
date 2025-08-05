1975ad0bbaf784a0a76fa5115023211106e8f1d6
Joaquin Stur <joaquinstur@gmail.com>
2025-08-05 12:00:10 -0300

    Crea componente Navbar
    draft

---
+++ project/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { GiFishingLure } from "react-icons/gi"
+
+export function Navbar (){
+  return(
+    <header className="sticky top-0 bg-teal-700 shadow-md">
+      <div>
+        <div>
+          <GiFishingLure className='h-8 w-8 text-yellow-500' />
+        </div>
+        <nav>
+
+        </nav>
+      </div>
+    </header>
+  )
+}
