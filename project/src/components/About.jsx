cacf9a42812b42cf3ffa1902d6672dbecab56362
Joaquin Stur <joaquinstur@gmail.com>
2025-08-07 20:24:26 -0300

    estilizacion About

--- project/src/components/About.jsx
+++ project/src/components/About.jsx
@@ -8,25 +8,25 @@ export function About (){
           <div className="w-[300px] h-[400px] bg-[url('/images/Guia1.png')] bg-cover bg-center rounded-lg"></div>
           <div className="w-[300px] h-[400px] bg-[url('/images/Guia2.png')] bg-cover bg-center rounded-lg"></div>
         </div>
-        <div className="w-full md:w-2/3 flex flex-col justify-center text-center">
-          <h3 className="text-3xl font-bold text-gray-800 mb-4">
+        <div className="w-full md:w-2/3 flex flex-col justify-center text-center shadow-xl rounded ring-3 ring-teal-700 bg-amber-400 p-8 h-full self-center">
+          <h3 className="text-3xl font-bold mb-4">
             Los Verdaderos Profesionales de la Pesca Deportiva
           </h3>
-          <p className="text-lg text-gray-800">
+          <p className="text-lg">
             Somos guías con años de experiencia en las aguas más ricas y turbulentas del país. Bajo nuestra ala, podrás ser uno de nosotros, pescar y sentir como nosotros, para capturar tu propio monstruo de rio.
           </p>
           <div className="py-4 grid gap-4 text-left">
             <div className="flex gap-1">
-              <FaLongArrowAltRight className='h-6 w-6 text-yellow-600 shrink-0' />
+              <FaLongArrowAltRight className='h-6 w-6 text-teal-700 shrink-0' />
               <div className="flex flex-col">
                 <h3 className="px-2 font-bold">Juan Fisher:</h3>
                 <span className="text-left">Campeón mundial de pesca 5 años consecutivos (2015-16-17-18-19). Según expertos, nadie maneja la caña como el. Donde pone el ojo, pone el anzuelo, un francotirador del rio. Con sus indicaciones vas a clavar la caña donde quieras. </span>
               </div>
             </div>
             <div className="flex gap-1">
-              <FaLongArrowAltRight className='h-6 w-6 text-yellow-600 shrink-0' />
+              <FaLongArrowAltRight className='h-6 w-6 text-teal-700 shrink-0' />
               <div className="flex flex-col">
-                <h3 className="px-2 font-bold">Pietro Placido Peterson:</h3>
+                <h3 className="px-2 font-bold">Pietro Pedro Peterson:</h3>
                 <span className="text-left">Criado por nutrias, abandonado como niño en una isla, creció prácticamente en el rio. No importa el clima, profundidad u hora del dia, Pietro puede rastrear un pez como si fuese un tiburón. Ademas, con sus conocimientos de la fauna local, siempre es un lujo escucharlo.</span>
               </div>
             </div>
