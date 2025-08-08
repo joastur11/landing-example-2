1b10a7f2bdf4b660a5af2c9b7e9d2c8d6cfa7d86
Joaquin Stur <joaquinstur@gmail.com>
2025-08-08 10:45:21 -0300

    crea seccion reviews

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -2,6 +2,7 @@ import { About } from "./components/About"
 import { Circuits } from "./components/Circuits"
 import { Hero } from "./components/Hero"
 import { Navbar } from "./components/Navbar"
+import { Reviews } from "./components/Reviews"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <About />
       <Circuits />
+      <Reviews />
     </>
   )
 }
---
+++ project/src/components/Reviews.jsx
@@ -0,0 +1,7 @@
+export function Reviews (){
+  return(
+    <section id="reviews" className="min-h-screen flex flex-col items-center justify-center bg-orange-200">
+      <div>holaxd</div>
+    </section>
+  )
+}
